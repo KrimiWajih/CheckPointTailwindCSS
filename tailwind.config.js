f373d00f2cefb82cf026e3f95303cb1209a22ab0
KrimiWajih <Krimi.wajih@gmail.com>
2025-01-13 23:34:18 +0100

    CheckPointTailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        peach: "#fff7f0",
+        violett: "#755cde",
+        orange1: "#f5a560",
+        roze1: "#f39d9e",
+        orange2: "#ea7564",
+        vertblue: "#62c4b7",
+        violetbrown: "#55204a",
+        // Add a descriptive name for your color
+      },
+    },
+  },
+  plugins: [],
+};
